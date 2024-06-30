cdc85343cf4b4e8e907d551f80da3d622ce17ec5
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-06-30 00:02:42 -0300

    Actualizar plugins

---
+++ plugins/tools-uppercase.js
@@ -0,0 +1,15 @@
+var handler = async (m, { text, conn }) => {
+    if (!text) return conn.reply(m.chat, `🎌 *Ingrese el texto que quiere convertir a mayúsculas*\n\nEjemplo, !uppercase [texto]`, m);
+
+    const uppercasedText = text.toUpperCase();
+    conn.reply(m.chat, `🔠 *Texto en mayúsculas*:\n\n${uppercasedText}`, m);
+};
+
+handler.help = ['uppercase'];
+handler.tags = ['tools'];
+handler.command = /^uppercase$/i;
+
+handler.register = true;
+handler.limit = true;
+
+export default handler;
