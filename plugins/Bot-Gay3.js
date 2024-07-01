c69ad3a6d6908cf7561cd9a3f0932720ba58adc6
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-07-01 05:58:10 -0300

    Actualizar Bot-Gay3.js

--- plugins/Bot-Gay3.js
+++ plugins/Bot-Gay3.js
@@ -1,7 +1,7 @@
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 
-        if (!text) throw `🪷 𝐄𝐑𝐑𝐎𝐑 🪷 𝐢𝐧𝐠𝐫𝐞𝐬𝐚 𝐞𝐥 𝐭𝐢𝐭𝐮𝐥𝐨 𝐝𝐞 𝐮𝐧𝐚 𝐜𝐚𝐧𝐜𝐢𝐨𝐧\n\n[ 📌 ] 𝐞𝐣𝐞𝐦𝐩𝐥𝐨 *${usedPrefix + command}* 𝐋𝐢𝐥 𝐏𝐞𝐞𝐩 𝐡𝐚𝐭𝐞 𝐦𝐲 𝐥𝐢𝐟𝐞`
+        if (!text) throw `🪷 𝐄𝐑𝐑𝐎𝐑 🪷 𝐢𝐧𝐠𝐫𝐞𝐬𝐚 𝐞𝐥 𝐭𝐢𝐭𝐮𝐥𝐨 𝐝𝐞 𝐮𝐧𝐚 𝐜𝐚𝐧𝐜𝐢𝐨𝐧\n\n[ 💫 ] 𝐞𝐣𝐞𝐦𝐩𝐥𝐨 *${usedPrefix + command}* 𝐋𝐢𝐥 𝐏𝐞𝐞𝐩 𝐡𝐚𝐭𝐞 𝐦𝐲 𝐥𝐢𝐟𝐞`
         let res = await yts(text)
         let vid = res.videos[0]
         if (!vid) throw `🚫 𝐀𝐓𝐄𝐍𝐂𝐈𝐎𝐍 🚫 ᥎і́ძᥱ᥆/ᥲᥙძі᥆ ᥒ᥆ ᥱᥒᥴ᥆ᥒ𝗍rᥲძ᥆`
