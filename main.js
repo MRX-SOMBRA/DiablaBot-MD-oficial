95e61f21678dbe9d73d951178a9e2e8005b11362
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-07-04 19:36:00 -0300

    Actualizar main.js

--- main.js
+++ main.js
@@ -312,7 +312,7 @@ console.log(chalk.yellow('⚠️ㅤEscanea este codigo QR, el codigo QR expira e
 if (connection == 'open') {
 await conn.groupAcceptInvite('LhlBSyakvI3GjGSvFTa1hh')
 console.log(chalk.bold.cyan('\n╭────────────── • DiablaBot-MD 🍂 • ─────────────◉\n│\n│Conectado correctamente al WhatsApp.\n│\n╰────────────── • DiablaBot-MD 🍂 • ─────────────◉\n'))}
-conn.fakeReply('5491168758497@s.whatsapp.net', '😄', '0@s.whatsapp.net', '😸 Soy NilouBot\nRecientemente Me E Conectado', '0@s.whatsapp.net')}
+//conn.fakeReply('5491168758497@s.whatsapp.net', '😄', '0@s.whatsapp.net', '😸 Soy NilouBot\nRecientemente Me E Conectado', '0@s.whatsapp.net')}
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
 if (reason == 405) { 
 await fs.unlinkSync("./sessions/" + "creds.json")
