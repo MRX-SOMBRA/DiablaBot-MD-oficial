f7ef35601c493cf3eefc420f342436a917e1fb61
Enzo-PL-Oficial <106297033+EnzoVaselevich@users.noreply.github.com>
2024-07-04 11:09:59 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -9,7 +9,8 @@ import { fileURLToPath, pathToFileURL } from 'url'
 
  global.owner = [
 ['5491168758497', '🍁 𝗢𝘄𝗻𝗲𝗿 - 𝗖𝗹𝗲𝗮𝘁𝗼𝗿 🐼', true],
-['5492215034412', '🪷 𝐌𝐚𝐭𝐢𝐬𝐬 - 𝐂𝐫𝐢𝐩𝐭𝐨𝐎𝐅𝐂 🌠', true]]
+['5492215034412', '🪷 𝐌𝐚𝐭𝐢𝐬𝐬 - 𝐂𝐫𝐢𝐩𝐭𝐨𝐎𝐅𝐂 🌠', true];
+['527561175278', 'Matias2 Owner', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
