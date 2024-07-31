d6c29d44eb4c8dd9c557b63bb357994a679f9fcf
MRX-SOMBRA <efrainedwint@gmail.com>
2024-07-31 07:56:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
  global.owner = [
-['5491168758497', '🍁 𝗢𝘄𝗻𝗲𝗿 - 𝗖𝗹𝗲𝗮𝘁𝗼𝗿 🐼', true],
-['5492215034412', '🪷 𝐌𝐚𝐭𝐢𝐬𝐬 - 𝐂𝐫𝐢𝐩𝐭𝐨𝐎𝐅𝐂 🌠', true],
-['527561175278', 'Matias2 Owner', true]]
+[51964177009', '🍁 Sombra - 𝗖𝗹𝗲𝗮𝘁𝗼𝗿 🐼', true],
+[51964177009', '🪷 Sombra - 𝐂𝐫𝐢𝐩𝐭𝐨𝐎𝐅𝐂 🌠', true],
+[51964177009', 'Sombra Owner', true]]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
+global.botNumberCode = '' //Ejemplo: +51964177009
 global.confirmCode = ''
 
 global.suittag = ['5214531287294']
@@ -22,7 +22,7 @@ global.mods = []
 global.prems = []
 
 //Solo desarrolladores aprobados
-global.isdev = [ /* ['5217294888993'], */ ['5214434703586'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['5492964650915'], ['50558124470'], ['573012482597']]
+global.isdev = [ /* [51964177009'], */ [51964177009'], [51964177009'], [51964177009'], ['51964177009'], [51964177009'], [51964177009'], [51964177009'], [51964177009'], ['51964177009'], [51964177009']]
 
 global.packname = '🌸 ᴰⁱᵃᵇˡⁱᵗᵃᴮᵒᵗ⁻ᴹᴰ 🐼'
 global.author = 'Enzito'
